916548395d42ba2c3bb7638a0cb9b26b38c5dbaf
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-12-14 21:23:29 -0300

    algorithms display refactor

--- src/main/java/br/com/sptech/App.java
+++ src/main/java/br/com/sptech/App.java
@@ -1,25 +1,34 @@
 import java.util.Arrays;
 
+import algorithms.*;
+
 public class App {
     public static void main(String[] args) {
         Integer[] list = { 4, 2, 8, 7, 5, 5, 2, 8, 3, 7, 23, 9, 2, 5, 8, 5, 3, 8, 3, 3, 8, 3, 8 };
-        long start = System.nanoTime();
-        String sortedList = Arrays.toString(Insertion.sort(list));
-        long end = System.nanoTime();
-        System.out.println(String.format(
-                "The Insertion algorithm returned: %s \nand took %g milliseconds to complete execution\n", sortedList,
-                (end - start) / 1_000_000d));
-        start = System.nanoTime();
-        sortedList = Arrays.toString(Selection.sort(list));
-        end = System.nanoTime();
-        System.out.println(String.format(
-                "The Selection algorithm returned %s \nand took %g milliseconds to complete execution\n", sortedList,
-                (end - start) / 1_000_000d));
-        start = System.nanoTime();
-        sortedList = Arrays.toString(Bubble.sort(list));
-        end = System.nanoTime();
-        System.out.println(String.format(
-                "The Bubble algorithm returned %s \nand took %g milliseconds to complete execution\n", sortedList,
-                (end - start) / 1_000_000d));
+        displayAll(list);
+    }
+
+    public static void displayAll(Integer[] list) {
+        for (int i = 0; i < 4; i++) {
+            // loop needed as the list, in this case the array, is being changed due to
+            // object memory reference issues
+            Integer[] array = Arrays.copyOfRange(list, 0, list.length);
+            switch (i) {
+                case 0:
+                    Insertion.displaySorted(array);
+                    break;
+                case 1:
+                    Selection.displaySorted(array);
+                    break;
+                case 2:
+                    Bubble.displaySorted(array);
+                    break;
+                case 3:
+                    Merge.displaySorted(array);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
